096ab4ae2469147a012d9759c9f172bd7d7c1b7e
Joseph Cameron <jf.cameron@gmail.com>
2018-04-19 22:37:50 -0400

    wip

--- workspace/build.sh
+++ workspace/build.sh
@@ -20,9 +20,10 @@ cd $(getPathToScript)
 rm -R src
 
 cmake \
--DJFC_BUILD_APP=ON  \
 -DJFC_BUILD_DOCS=OFF \
--DJFC_BUILD_TESTS=ON  \
+-DJFC_BUILD_TESTS=ON \
+-DJFC_BUILD_APP=ON \
 -DJFC_AUTO_RUN_APP=OFF \
+J-DFC_FORMAT_CODE=OFF \
  \
 .. && make
